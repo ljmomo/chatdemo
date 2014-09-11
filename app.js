85c5f155a39686d15d9e394362bd83722a06f59f
momoo <568258406@qq.com>
2014-09-11 11:49:50 +0800

    '提交'

---
+++ app.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+io.set('log level', 1); 
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+app.get('/', function(req, res){
+  res.sendfile('views/chat.html');
+});
+
+ //WebSocket连接监听
+io.on('connection', function (socket) {
+  console.log('已有链接上来');
+  socket.emit('open');//通知客户端已连接
+
+  // 打印握手信息
+  // console.log(socket.handshake);
+
+  // 构造客户端对象
+  var client = {
+    socket:socket,
+    name:false,
+    color:getColor()
+  }
+
+  // 对message事件的监听
+  socket.on('message', function(msg){
+    var obj = {time:getTime(),color:client.color};
+
+    // 判断是不是第一次连接，以第一条消息作为用户名
+    if(!client.name){
+        client.name = msg;
+        obj['text']=client.name;
+        obj['author']='System';
+        obj['type']='welcome';
+        console.log(client.name + ' login');
+
+        //返回欢迎语
+        socket.emit('system',obj);
+        //广播新用户已登陆
+        socket.broadcast.emit('system',obj);
+     }else{
+
+        //如果不是第一次的连接，正常的聊天消息
+        obj['text']=msg;
+        obj['author']=client.name;      
+        obj['type']='message';
+        console.log(client.name + ' say: ' + msg);
+
+        // 返回消息（可以省略）
+        socket.emit('message',obj);
+        // 广播向其他用户发消息
+        socket.broadcast.emit('message',obj);
+      }
+    });
+
+    //监听出退事件
+    socket.on('disconnect', function () {  
+      var obj = {
+        time:getTime(),
+        color:client.color,
+        author:'System',
+        text:client.name,
+        type:'disconnect'
+      };
+
+      // 广播用户已退出
+      socket.broadcast.emit('system',obj);
+      console.log(client.name + 'Disconnect');
+    });
+
+});
+
+
+server.listen(3000, function() {
+  console.log('端口已经启动3000');
+});
+
+var getTime=function(){
+  var date = new Date();
+  return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+}
+
+var getColor=function(){
+  var colors = ['aliceblue','antiquewhite','aqua','aquamarine','pink','red','green',
+                'orange','blue','blueviolet','brown','burlywood','cadetblue'];
+  return colors[Math.round(Math.random() * 10000 % colors.length)];
+}
